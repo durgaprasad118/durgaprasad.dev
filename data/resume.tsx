b6bb4d71b154dc99e03add9e0949dbd196481f1c
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-06-20 23:59:44 +0530

    update in the projects and bio

--- data/resume.tsx
+++ data/resume.tsx
@@ -30,7 +30,7 @@ export const DATA = {
         tel: '+917661044831',
         social: {
             GitHub: {
-                url: 'https://github.com/durgaprsad118',
+                url: 'https://github.com/durgaprasad118',
                 icon: Icons.github
             },
             LinkedIn: {
@@ -43,17 +43,16 @@ export const DATA = {
             }
         }
     },
-
     work: [
         {
             company: 'Celebal Technologies',
             href: 'https://celebaltech.com/',
-            badges: ['Gold'],
+            badges: [],
             location: 'Remote',
             title: 'Frontend Devloper Intern',
             logoUrl: '/celebal.jpeg',
             start: 'May 2023',
-            end: 'August 2023',
+            end: 'Aug 2023',
             description:
                 'Developed key features and improved user interfaces for a dynamic web application using React JS. Gained hands-on experience in state management using Redux Toolkit and successfully integrated third-party APIs.'
         }
@@ -61,110 +60,72 @@ export const DATA = {
     education: [
         {
             school: 'Indian Institute of Engineering Science and Technology, Shibpur',
-            href: 'https://uwaterloo.ca',
+            href: 'https://www.iiests.ac.in/',
             degree: 'Mechanical Engineering',
             logoUrl: '/iiest.png',
-            start: '2016',
-            end: '2021'
+            start: '2020',
+            end: '2024'
         }
     ],
     projects: [
         {
-            title: 'Chat Collect',
-            href: 'https://chatcollect.com',
-            dates: 'Jan 2024 - Feb 2024',
+            title: 'Ink Well',
+            href: 'https://blogging-app-mauve.vercel.app/',
+            dates: 'March 2024 - April 2024',
             active: true,
             description:
-                'With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.',
+                'Engineered a full-stack, performance-optimized **blogging platform** with secure authentication, blog management, WYSIWYG editor, and responsive design using modern technologies, deployed serverlessly.',
             technologies: [
-                'Next.js',
+                'React',
                 'Typescript',
                 'PostgreSQL',
                 'Prisma',
                 'TailwindCSS',
-                'Stripe',
-                'Shadcn UI',
-                'Magic UI'
+                'Hono',
+                'recoil'
             ],
             links: [
                 {
-                    type: 'Website',
-                    href: 'https://chatcollect.com',
-                    icon: <Icons.globe className="size-3" />
+                    type: 'Live Preview',
+                    href: 'https://blogging-app-mauve.vercel.app/',
+                    icon: <Icons.globe className="size-1" />
                 },
                 {
                     type: 'Source',
-                    href: 'https://github.com/magicuidesign/magicui',
-                    icon: <Icons.github className="size-3" />
+                    href: 'https://github.com/durgaprasad118/Blogging_App',
+                    icon: <Icons.gitti className="size-1" />
                 }
             ],
-            image: '/dp1.jpg',
-            video: 'https://drive.google.com/file/d/1EMMF6A8iLHkdPPSjkmMtbemeU_-3q5Ow/'
+            image: '/inkwell.png'
         },
         {
-            title: 'Magic UI',
-            href: 'https://magicui.design',
-            dates: 'June 2023 - Present',
+            title: 'Pro Manage',
+            href: '',
+            dates: 'Jan 2024 - March 2024',
             active: true,
             description:
-                'Designed, developed and sold animated UI components for developers.',
+                'Full-stack kanban task management app with secure authentication, advanced features, subtasks, due dates, real-time tracking, and optimized performance using modern tech stack.',
             technologies: [
-                'Next.js',
-                'Typescript',
-                'PostgreSQL',
-                'Prisma',
-                'TailwindCSS',
-                'Stripe',
-                'Shadcn UI',
-                'Magic UI'
+                'React',
+                'CSS',
+                'MongoDB',
+                'Node.js',
+                'express',
+                'RTK Query'
             ],
             links: [
                 {
-                    type: 'Website',
-                    href: 'https://magicui.design',
+                    type: 'Live Preview',
+                    href: 'https://pro-manage-kohl.vercel.app/',
                     icon: <Icons.globe className="size-2" />
                 },
                 {
                     type: 'Source',
-                    href: 'https://github.com/magicuidesign/magicui',
-                    icon: <Icons.github className="size-2" />
+                    href: 'https://github.com/durgaprasad118/Pro-Manage',
+                    icon: <Icons.gitti className="size-3" />
                 }
             ],
-            image: '',
-            video: 'https://cdn.magicui.design/bento-grid.mp4'
-        },
-        {
-            title: 'llm.report',
-            href: 'https://llm.report',
-            dates: 'April 2023 - September 2023',
-            active: true,
-            description:
-                'Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.',
-            technologies: [
-                'Next.js',
-                'Typescript',
-                'PostgreSQL',
-                'Prisma',
-                'TailwindCSS',
-                'Shadcn UI',
-                'Magic UI',
-                'Stripe',
-                'Cloudflare Workers'
-            ],
-            links: [
-                {
-                    type: 'Website',
-                    href: 'https://llm.report',
-                    icon: <Icons.globe className="size-3" />
-                },
-                {
-                    type: 'Source',
-                    href: 'https://github.com/dillionverma/llm.report',
-                    icon: <Icons.github className="size-3" />
-                }
-            ],
-            image: '',
-            video: 'https://cdn.llm.report/openai-demo.mp4'
+            image: '/promanage.png'
         }
     ]
 } as const;
