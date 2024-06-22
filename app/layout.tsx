00384d0679247f03a1d3a6ceb9ad0b362b119edb
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-06-22 09:54:54 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Inter as FontSans } from 'next/font/google';
-import { DATA } from '../data/resume';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
-import { cn } from '../lib/util';
-import { ThemeProvider } from '../components/theme-provider';
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import Head from 'next/head';
 import Navbar from '../components/navbar';
+import { ThemeProvider } from '../components/theme-provider';
+import { DATA } from '../data/resume';
+import { cn } from '../lib/util';
+import './globals.css';
 const fontSans = FontSans({
     subsets: ['latin'],
     variable: '--font-sans'
@@ -54,6 +55,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <body
                 className={cn(
                     'min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6',
