22b62630b462e7bbe58507fcd5dc5691885b9b15
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-06-22 09:02:43 +0530

    final update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
-
+import typography from '@tailwindcss/typography';
 const config = {
     darkMode: ['class'],
     content: [
@@ -78,10 +78,7 @@ const config = {
             }
         }
     },
-    plugins: [
-        require('tailwindcss-animate'),
-        require('@tailwindcss/typography')
-    ]
+    plugins: [require('tailwindcss-animate'), typography]
 } satisfies Config;
 
 export default config;
