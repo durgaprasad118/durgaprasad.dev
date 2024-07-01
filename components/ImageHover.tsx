b9f5f1e7024ee12e490d50d378b571c2a8260c5d
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-01 10:52:40 +0530

    Image hover effect added

--- components/ImageHover.tsx
+++ components/ImageHover.tsx
@@ -12,7 +12,7 @@ const ImageHoverComponent: React.FC<ImageHoverComponentProps> = ({
 }) => {
     return (
         <div className="overflow-hidden aspect-video cursor-pointer rounded-xl relative  group">
-            <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100  transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
+            <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100   transition-all duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
                 <div>
                     <div className="px-1  text-xl text-slate-100 group-hover:opacity-100 group-hover:translate-y-0 translate-y-1  pb-4 transform transition duration-300 ease-in-out">
                         <div className=" items-center text-sm flex flex-row gap-1">
